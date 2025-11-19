a2c716318653a427d3d3ab89a513324f6b3d5f43
H-strangeone <hiteshgitman@gmail.com>
2025-11-19 20:22:44 +0530

    initial vulnerable commit

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+import yaml
+import requests
+
+app = Flask(__name__)
+
+@app.route("/unsafe_yaml", methods=["POST"])
+def unsafe_yaml():
+    # Vulnerable to YAML deserialization attack
+    data = yaml.load(request.data, Loader=yaml.Loader)
+    return {"loaded": str(data)}
+
+@app.route("/")
+def index():
+    return "Hello from vulnerable demo!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
